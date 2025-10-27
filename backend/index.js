3068f3279f0c710b5d1138f69dd4b2d65778446a
Diptipathak2007 <diptipathak019@gmail.com>
2025-10-27 11:00:23 +0530

    completed backend setup

--- backend/index.js
+++ backend/index.js
@@ -7,8 +7,36 @@ import morgan from 'morgan';
 dotenv.config();
 
 const app = express();
+app.use(cors({
+  origin: process.env.FRONTEND_URL ,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders:['Content-Type', 'Authorization']
+}));
+app.use(morgan('dev'));
+// MongoDB connection
+mongoose.connect(process.env.MONGODB_URI, {}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((err) => {
+  console.error('Error connecting to MongoDB:', err.message);
+});
+
 app.use(express.json());
 const PORT = process.env.PORT || 8081;
+
+app.get("/",(req, res) => {
+  res.status(200).json({ message: "Server is up and running!" });
+});
+
+//error middleware
+app.use((err,req,res,next)=>{
+  console.error(err.stack);
+  res.status(500).json({message: "Internal Server Error"});
+})
+
+//not found middleware
+app.use((req,res,next)=>{
+  res.status(404).json({message: "Route not found"});
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
