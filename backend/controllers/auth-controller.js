18a137ff9a09cf695d99c01e91fbb03a6ccd2f5b
Diptipathak2007 <diptipathak019@gmail.com>
2025-10-29 23:15:35 +0530

    auth controller

--- backend/controllers/auth-controller.js
+++ backend/controllers/auth-controller.js
@@ -1,4 +1,32 @@
-const registerUser = async (req, res) => {};
-const loginUser = async (req, res) => {};
+import User from "../models/user-model.js";
+import bcrypt from "bcryptjs";
+const registerUser = async (req, res) => {
+  try {
+    const { email, name, password } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const newUser = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+    
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+const loginUser = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
 
 export { registerUser, loginUser };
